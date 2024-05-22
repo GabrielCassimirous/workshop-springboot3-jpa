98dfb62713103fb7bdb9d6a6efad663148fb7d1f
Gabriel Cassimiro <gabrielcassimirous@gmail.com>
2024-05-22 13:23:41 -0300

    Product

--- src/main/java/com/Cassimirous/course/config/TestConfig.java
+++ src/main/java/com/Cassimirous/course/config/TestConfig.java
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.Configuration;
 
 import com.Cassimirous.course.entities.Category;
 import com.Cassimirous.course.entities.Order;
+import com.Cassimirous.course.entities.Product;
 import com.Cassimirous.course.entities.User;
 import com.Cassimirous.course.entities.enums.OrderStatus;
 import com.Cassimirous.course.repositories.CategoryRepository;
 import com.Cassimirous.course.repositories.OrderRepository;
+import com.Cassimirous.course.repositories.ProductRepository;
 import com.Cassimirous.course.repositories.UserRepository;
 
 @Configuration
@@ -27,6 +29,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -34,7 +39,14 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books"); 
 		Category cat3 = new Category(null, "Computers"); 
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
---
+++ src/main/java/com/Cassimirous/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.Cassimirous.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Cassimirous.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
